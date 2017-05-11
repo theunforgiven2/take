35695dd6f66166c91eadabea885d95a7c6d72375
Krzysztof Polański (krzypol515) <krzypol515@student.polsl.pl>
2017-05-12 00:35:47 +0200

    Test commit

---
+++ takeBiuro/src/takeBiuro/Test.java
@@ -0,0 +1,10 @@
+package takeBiuro;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jestem zjeban¹ klas¹, elo");
+	}
+
+}

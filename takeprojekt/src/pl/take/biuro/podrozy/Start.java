8b68a0e87ad65dee7ecccd00f3e351c128713536
Krzysztof Polański (krzypol515) <krzypol515@student.polsl.pl>
2017-05-14 12:37:08 +0200

    Gówno

--- takeprojekt/src/pl/take/biuro/podrozy/Start.java
+++ takeprojekt/src/pl/take/biuro/podrozy/Start.java
@@ -2,10 +2,6 @@ package pl.take.biuro.podrozy;
 
 public class Start {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Elo menelo"); // jo
-=======
-		System.out.println("Elo");
->>>>>>> parent of 722c8b3... Dodatkowy push
 	}
 }

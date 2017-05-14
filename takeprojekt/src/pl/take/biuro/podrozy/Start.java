589c7656340cd3935a339927524676f904b3bcab
Krzysztof Polański (krzypol515) <krzypol515@student.polsl.pl>
2017-05-14 12:32:18 +0200

    Revert "Dodatkowy push"
    This reverts commit 722c8b32d02a5e02d2a68c855af78b8b82cb8a97.
    
    # Conflicts:
    #	takeprojekt/src/pl/take/biuro/podrozy/Start.java

--- takeprojekt/src/pl/take/biuro/podrozy/Start.java
+++ takeprojekt/src/pl/take/biuro/podrozy/Start.java
@@ -2,6 +2,10 @@ package pl.take.biuro.podrozy;
 
 public class Start {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Elo menelo"); // jo
+=======
+		System.out.println("Elo");
+>>>>>>> parent of 722c8b3... Dodatkowy push
 	}
 }

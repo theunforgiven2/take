90b291d3fac972453432e658c21f31faa4516baa
Pawel <pawel190x@gmail.com>
2017-09-10 18:11:53 +0200

    Test dodania czegokolwiek do bazy

--- takeprojekt/src/pl/take/biuro/podrozy/Start.java
+++ takeprojekt/src/pl/take/biuro/podrozy/Start.java
@@ -1,7 +1,21 @@
 package pl.take.biuro.podrozy;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Start {
 	public static void main(String[] args) {
-		System.out.println("Blblblbl");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("takeprojekt");
+		EntityManager em = factory.createEntityManager();
+		
+		Miejsce mp = new Miejsce();
+		em.getTransaction().begin();
+		mp.setNazwa("NazwaMiejsca");
+		mp.setOpis("opisMiejsca");
+		em.persist(mp);
+		em.getTransaction().commit();
+		em.close();
+		//System.out.println("Blblblbl");
 	}
 }
